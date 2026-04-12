c47b1830672d061880eb8b2eed68b7f554fee30c
vaibhav9058 <vaibhavkubc@gmail.com>
2026-04-12 18:19:01 +0530

    few changes in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+
 import Login from "./pages/Login"
 import Profile from "./pages/Profile"
 import Dashboard from "./pages/Dashboard"
